85dc4d2a13e7de9165b84e73c1cef6d706188efc
Aidan R <aidanrolle2@gmail.com>
2025-05-20 16:20:19 -0400

    Implemented both actor & critic models

--- models.py
+++ models.py
@@ -15,3 +15,41 @@ Class (Critic)
     out:    Reward
 * Trained on replay buffer of Actor *
 '''
+
+
+class Actor(nn.Module):
+    def __init__(self):
+        super(Actor, self).__init__()
+        
+        self.l1 = nn.Linear(17, 32)
+        self.l2 = nn.Linear(32, 64)
+        self.l3 = nn.Linear(64, 32)
+        self.l4 = nn.Linear(32, 16)
+        self.l5 = nn.Linear(16, 2)
+    
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = F.relu(self.l3(x))
+        x = F.relu(self.l4(x))
+        return self.l5(x)
+        
+
+class Critic(nn.Module):
+    def __init__(self):
+        super(Critic, self).__init__()
+        
+        self.l1 = nn.Linear(365, 256)
+        self.l2 = nn.Linear(256, 128)
+        self.l3 = nn.Linear(128, 128)
+        self.l4 = nn.Linear(128, 64)
+        self.l5 = nn.Linear(64, 32)
+        self.l6 = nn.Linear(32, 1)
+        
+    def forward(self, x):
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = F.relu(self.l3(x))
+        x = F.relu(self.l4(x))
+        x = F.relu(self.l5(x))
+        return self.l6(x)
