75cadcc490d999e9e307a4703f1c336bc031109b
Aidan R <aidanrolle2@gmail.com>
2025-05-20 16:53:02 -0400

    Replay buffer class and functions

---
+++ buffer.py
@@ -0,0 +1,21 @@
+class Buffer:
+    def __init__(self, size):
+        self.buffer = []
+        self.size   = size
+    
+    def add(self, experience):
+        if not Buffer.isValid(experience):
+            return
+        
+        if len(self.buffer) >= self.size:
+            self.buffer.pop(0)
+            
+        self.buffer.append(experience)
+    
+    def isValid(experience):
+        if experience[3] is "terminated":
+            return True
+        return False
+    
+    def get(self):
+        return self.buffer
