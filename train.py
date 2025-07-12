51c08923dccfcf4a187ac56f9c4cc180b947691d
Aidan R <aidanrolle2@gmail.com>
2025-07-11 21:38:10 -0400

    Very rough draft of training

--- train.py
+++ train.py
@@ -6,18 +6,54 @@ from models import Actor, Critic
 from buffer import Buffer
 
 
-'''
-Training
 
-for trial
-    action = sample(actor.forward(state))
-    get new observation
-    add expeirence to replay buffer
+def Initialize():
+    return Actor, Critic, Buffer
     
-    if condition:
-        set = torch (get random dataset from buffer)
-        train critic with set
-        
-    update 
 
-'''
+def updatable():
+    pass
+
+
+def update(buffer, n, exs):
+    for _ in range(n):
+        # randomly sample buffer
+        batch = buffer.sample(exs)
+        
+        '''
+        compute targets for Q functions
+        update q functions (gradient descent)
+        update policy (gradient ascent)
+        update target network        
+        '''
+    pass
+
+
+def main(converging=True):
+    
+    model1, model2, buffer = Initialize()
+    
+    # Initialize Enviornment
+    
+    while converging:
+        # select Action a for State s w/ Actor model
+        
+        # take action in the enviornment
+        
+        if not terminal:
+            # store old state, action, reward, new state, and new state terminality
+            buffer.add("(s, a, r, s', t)")
+        else:
+            # reset enviornment
+            
+        if updatable():
+            update(numOfUpdates)
+            
+            
+    # Save trained models
+    
+    return
+
+
+if __name__ == "__main__":
+    pass
